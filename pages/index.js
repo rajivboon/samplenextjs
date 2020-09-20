e2090bfd2a53dc8ed8e2a02fec5895b212b0a897
rajivboon <vrajivboon@gmail.com>
2020-09-20 23:06:08 +0530

    start

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Index = () => {
+    return (
+        <div>
+            <h1>Hello from next js </h1>
+            <p> discription of this site </p>
+        </div>
+    )
+}
+
+export default Index
